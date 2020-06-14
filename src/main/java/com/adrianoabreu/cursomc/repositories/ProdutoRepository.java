2bc9e5d2415723673b12b6b2acab54bfc553d48d
Adriano Luis Machado Abreu <adriano.luis.machado2013@hormail.com>
2020-06-14 19:02:44 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/adrianoabreu/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.adrianoabreu.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.adrianoabreu.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
