f587de37027ed36c33e903676f1f6a73d55a90b6
Adriano Luis Machado Abreu <adriano.luis.machado2013@hormail.com>
2020-06-16 19:00:39 -0300

    Estado e Cidade

--- src/main/java/com/adrianoabreu/cursomc/CursomcApplication.java
+++ src/main/java/com/adrianoabreu/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.adrianoabreu.cursomc.domain.Categoria;
+import com.adrianoabreu.cursomc.domain.Cidade;
+import com.adrianoabreu.cursomc.domain.Estado;
 import com.adrianoabreu.cursomc.domain.Produto;
 import com.adrianoabreu.cursomc.repositories.CategoriaRepository;
+import com.adrianoabreu.cursomc.repositories.CidadeRepository;
+import com.adrianoabreu.cursomc.repositories.EstadoRepository;
 import com.adrianoabreu.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -21,6 +25,12 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -44,6 +54,19 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 
 }
---
+++ src/main/java/com/adrianoabreu/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,12 @@
+package com.adrianoabreu.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.adrianoabreu.cursomc.domain.Categoria;
+import com.adrianoabreu.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
