dde620cde38fbebdc008e00284b0b5867ee88b7d
Adriano Luis Machado Abreu <adriano.luis.machado2013@hormail.com>
2020-06-17 18:10:58 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/adrianoabreu/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.adrianoabreu.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.adrianoabreu.cursomc.domain.Cliente;
+import com.adrianoabreu.cursomc.repositories.ClienteRepository;
+import com.adrianoabreu.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
