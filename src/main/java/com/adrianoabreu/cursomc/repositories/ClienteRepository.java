a1532fed1089f19069cf95c38801d03fd15b8f2a
Adriano Luis Machado Abreu <adriano.luis.machado2013@hormail.com>
2020-06-16 21:32:42 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/adrianoabreu/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,12 @@
+package com.adrianoabreu.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.adrianoabreu.cursomc.domain.Categoria;
+import com.adrianoabreu.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
---
+++ src/main/java/com/adrianoabreu/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,12 @@
+package com.adrianoabreu.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.adrianoabreu.cursomc.domain.Categoria;
+import com.adrianoabreu.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
