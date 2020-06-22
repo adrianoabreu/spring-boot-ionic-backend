a97a76f78efdc1a86c083356538bcfcb85cde259
Adriano Luis Machado Abreu <adriano.luis.machado2013@hormail.com>
2020-06-22 20:44:01 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/adrianoabreu/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/adrianoabreu/cursomc/dto/CategoriaDTO.java
@@ -3,11 +3,16 @@ package com.adrianoabreu.cursomc.dto;
 import java.io.Serializable;
 
 import com.adrianoabreu.cursomc.domain.Categoria;
+import javax.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Length;
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 
 	public CategoriaDTO() {
--- src/main/java/com/adrianoabreu/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/adrianoabreu/cursomc/resources/CategoriaResource.java
@@ -33,7 +33,8 @@ public class CategoriaResource {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDto) {
+		Categoria obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
@@ -41,7 +42,8 @@ public class CategoriaResource {
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
-	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDto, @PathVariable Integer id) {
+		Categoria obj = service.fromDTO(objDto);
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
