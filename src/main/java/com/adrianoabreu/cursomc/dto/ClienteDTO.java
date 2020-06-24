d841977aad041f846b577b224d7fcd62091caad8
Adriano Luis Machado Abreu <adriano.luis.machado2013@hormail.com>
2020-06-24 20:08:07 -0300

    Validacao customizada de CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/adrianoabreu/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/adrianoabreu/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.adrianoabreu.cursomc.domain.Cliente;
+import com.adrianoabreu.cursomc.services.validation.ClienteInsert;
 
+@ClienteInsert
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
