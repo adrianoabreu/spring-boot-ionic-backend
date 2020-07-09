73f1ca231868968b90ad735b87567221a562f20f
Adriano Luis Machado Abreu <adriano.luis.machado2013@hormail.com>
2020-07-08 22:36:14 -0300

    Email HTML

--- src/main/java/com/adrianoabreu/cursomc/services/PedidoService.java
+++ src/main/java/com/adrianoabreu/cursomc/services/PedidoService.java
@@ -67,7 +67,7 @@ public class PedidoService {
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
 		//System.out.println(obj);
-		emailService.sendOrderConfirmationEmail(obj);
+		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
 
