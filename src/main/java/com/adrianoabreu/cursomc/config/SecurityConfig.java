965f40d2e366a51c0eb2f796b042976ae790815e
Adriano Luis Machado Abreu <adriano.luis.machado2013@hormail.com>
2020-07-12 21:56:29 -0300

    Implementando autorizacao

--- src/main/java/com/adrianoabreu/cursomc/config/SecurityConfig.java
+++ src/main/java/com/adrianoabreu/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.adrianoabreu.cursomc.security.JWTAuthenticationFilter;
+import com.adrianoabreu.cursomc.security.JWTAuthorizationFilter;
 import com.adrianoabreu.cursomc.security.JWTUtil;
 
 @Configuration
@@ -57,6 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
@@ -76,4 +78,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
 }
